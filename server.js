6d97deae71024fe9bf89e7b8da8f37668efc8ba5
Dylan Comeau <comeaudc@comcast.net>
2022-07-27 15:33:53 -0400

    Final Commit: All routes are completed

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3003;
-const fate = require('./models/magic.js')
+const choices = require('./models/magic.js')
 
 
 const fs = require('fs');
+const { parse } = require('path');
 app.engine('hypatia', (filePath, options, callback) => {
     fs.readFile(filePath, (err, content) => {
         if (err) return callback(err)
@@ -13,8 +14,11 @@ app.engine('hypatia', (filePath, options, callback) => {
         const rendered = content.toString()
         .replace('#title#', options.title)
         .replace('#message#', options.message)
+        .replace('#message1#', options.message1)
         .replace('#content#', options.content)
         .replace('#question#', options.question)
+        .replace('#outcome#', options.outcome)
+        .replace('#link#', options.link)
 
         return callback(null, rendered)
     });
@@ -23,12 +27,69 @@ app.engine('hypatia', (filePath, options, callback) => {
 app.set('views', './views');
 app.set('view engine', 'hypatia');
 
+app.get('/', (req, res) => {
+    let bugs;
+    if(Math.random() > 0.87){
+        bugs = (Math.floor(Math.random() * 10))
+        
+    }
+    else{
+        bugs = -1
+    }
+
+    res.render('passItAround', {
+        title:  `99 Bugs`,
+        message:    `99 little bugs in the code!`,
+        outcome:    `99 little bugs in the code!`,
+        message1:   `99 little buuuugs!!!`,
+        content:    `Take one down, patch it around!`,
+        link:       `<a href="http://localhost:3000/${99 + bugs}">See how your patch went?</a>`
+    })
+});
+
+app.get('/:number', (req, res) => {
+    let option1 = "See how your patch went?"
+    let option2 = "Start Over?"
+    let count = parseInt(req.params.number)
+    let bugs;
+
+    if(Math.random() > 0.87){
+    bugs = (Math.floor(Math.random() * 10))
+    
+    }
+    else{
+        bugs = -1
+    }
+    let next = count + bugs
+    if(count > 0){
+        res.render('passItAround', {
+            title:  `${count} Bugs`,
+            message:    `${count} little bugs in the code!`,
+            outcome:    `${count} little bugs in the code!`,
+            message1:   `${count} little buuuugs!!!`,
+            content:    `Take one down, patch it around!`,
+            link:       `<a href="http://localhost:3000/${next}">See how your patch went?</a>`
+        })        
+    }
+    else{
+        res.render('passItAround', {
+            title:  `No More!!`,
+            message:    `No more little bugs in the Code!`,
+            outcome:    `No more little bugs in the code!`,
+            message1:   `No more little buuuugs!!!`,
+            content:    `You fixed them all, banged your head on the wall`,
+            link:       `<a href="http://localhost:3000/">Patch All The Bugs Again?</a>`
+        })
+    }
+
+});
+
 app.get('/greeting', (req, res) => {
     res.render('template', {
         title: 'Greetings',
         message: 'Greetings!',
         content: 'Howdy there, Sheriff Stranger!!!!' 
-    })
+    });
 });
 
 app.get('/greeting/:firstName', (req, res) => {
@@ -36,8 +97,7 @@ app.get('/greeting/:firstName', (req, res) => {
         title: 'Greetings',
         message: 'Greetings!',
         content: `Howdy there, Sheriff ${req.params.firstName}`
-    }
-    )
+    });
 });
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
@@ -48,10 +108,11 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
         title:  'Tip Calc',
         message:    'Tip Calculator',
         content:    `Your ${req.params.tipPercentage}% tip on $${req.params.total} is $${tip}!`
-    })
+    });
 });
 
 app.get('/magic/:question', (req, res) => {
+    let fate = choices[Math.floor(Math.random() * (choices.length))]
     let question = (req.params.question).replace("%", ' ').replace('20', '')
      question = question.split('')
     question[0] = question[0].toUpperCase();
@@ -62,8 +123,8 @@ app.get('/magic/:question', (req, res) => {
         message:    'Magic 8 Ball: What is it you desire?',
         question:   question + '?',   
         content:    fate
-    })
-})
+    });
+});
 
 app.listen(port, () => {
     console.log('I am listening on port', port)
